14365aee5395ace8a6f75a7533a47bd99b9ecfe3
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-08-11 20:22:59 -0300

    feat(index): chamando "createPassword"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import prompt from "prompt";
 import createQRCode from "./service/qr-code/create.js";
+import createPassword from "./service/password/create.js";
 import mainPrompt from "./prompts/prompt-main.js"
+
 
 async function main() {
     prompt.get(mainPrompt, async (err,result) => {
-        if(result.select==1) await createQRCode()
-        if(result.select==2)console.log("Escolheu password")
+        if(result.select==1) await createQRCode();
+        if(result.select==2) await createPassword();
     });
 
     prompt.start();
