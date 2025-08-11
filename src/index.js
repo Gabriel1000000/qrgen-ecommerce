d6dfdbaf4b6d7c8cbcad8bdedfcf38b13a46293c
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-08-11 17:01:37 -0300

    feat(index): chamndo a função "creareQRCode"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import prompt from "prompt";
+import createQRCode from "./service/qr-code/create.js";
 import mainPrompt from "./prompts/prompt-main.js"
 
 async function main() {
     prompt.get(mainPrompt, async (err,result) => {
-        if(result.select==1)console.log("Escolheu qrCODE")
+        if(result.select==1) await createQRCode()
         if(result.select==2)console.log("Escolheu password")
     });
 
